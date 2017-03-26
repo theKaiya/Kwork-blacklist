d48fc096e546fde7e7fb941085dbafc2d8f4a263
theKaiya <theKaiya@ya.ru>
2017-03-26 22:58:46 +0300

    Added Report and People controllers

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'PageController@home');
+
+Route::get('/reviews', 'ReportController@showAll')->name('reviews_list');
+
+Route::get('/reviews/add', 'ReportController@showAll')->name('reviews_add');
+
+Route::get('/reviews/{id}', 'ReportController@show')->name('review_show');
+
+Route::get('/people', 'PeopleController@showAll')->name('people_list');
+
+Route::get('/people/{id}', 'PeopleController@show')->name('people_show');
+
+Route::any('/login', 'LoginController@show')->name('login');
+
+Route::any('/register', 'RegisterController@show')->name('register');
