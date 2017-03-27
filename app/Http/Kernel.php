a251f47a463c55b6cbbd93f0568f4617524fbc24
theKaiya <theKaiya@ya.ru>
2017-03-27 20:36:56 +0300

    Added admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+
+        'admin' => [
+          \App\Http\Middleware\AdminMiddleware::class,
+        ],
     ];
 
     /**
